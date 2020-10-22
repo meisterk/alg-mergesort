13f9185df1c09f5e5577f4aa5c7f5d8f549997fd
meisterk <franz.kohnle@sabel.com>
2020-10-22 16:42:15 +0200

    Add testcases

--- mergesort.py
+++ mergesort.py
@@ -66,6 +66,21 @@ class TestMergesort(unittest.TestCase):
         sortiert = [3, 4, 7, 8, 9, 12]
         self.assertEqual(mergesort(unsortiert), sortiert)
 
+    def test_mergesort_2(self):
+        unsortiert = [1, 2, 4, 4, 2, 1]
+        sortiert = [1, 1, 2, 2, 4, 4]
+        self.assertEqual(mergesort(unsortiert), sortiert)
+
+    def test_mergesort_3(self):
+        unsortiert = []
+        sortiert = []
+        self.assertEqual(mergesort(unsortiert), sortiert)
+
+    def test_mergesort_4(self):
+        unsortiert = [2]
+        sortiert = [2]
+        self.assertEqual(mergesort(unsortiert), sortiert)
+
     def test_merge_1(self):
         sortiert1 = [1, 2, 3, 4, 5]
         sortiert2 = [6, 7, 8, 9, 10]
